5b0dbda70f5ab78be7342c576bddf0001cadafef
Jozef <jozefw@sbcglobal.net>
2016-05-17 13:06:59 -0700

    add animation and better gaussian values

--- application.js
+++ application.js
@@ -2,22 +2,29 @@ $(document).ready(function() {
 
     setInterval(function() {
         $("#needle").each(function() {
-            var a = Math.floor(Math.random() * 270 + 1);
-            if (a <= 60) {
+            var a = Math.floor(Math.random() * 225 + 1);
+
+            if (a <= 80) {
                 $("#bkg").css('background-color', 'green');
             }
-            else if(a >= 61 && a <= 120){
+            else if(a >= 81 && a <= 160){
             	$('#bkg').css('background-color','orange');
 
             } else {$('#bkg').css('background-color','red')}
 
 
-            $(this).css(
-                'transform', 'rotate(' + a + 'deg)');
+            // $(this).css(
+            //     'transform', 'rotate(' + a + 'deg)');
+        	$(this).animate({borderSpacing: 90},{
+        		step: function(now,fx){
+        			$(this).css('transform','rotate('+now+'deg)');
+        		},
+        		duration:'slow'
+        	},'linear');
         });
 
 
 
 
-    }, 9);
+    }, 1000);
 });
